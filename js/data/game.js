489b5d9d674f0e99a1c36b74c95c5629f61b1bc2
RomankinNikita <kryaxtun@gmail.com>
2018-11-25 13:22:38 +0400

    test 8

--- js/data/game.js
+++ js/data/game.js
@@ -1,7 +1,8 @@
 const MAX_LIVES = 3;
-const ANSWER_NUMBER = 10;
+const NUMBER_OF_ANSWERS = 10;
 const LEFT_LIVES_POINT = 50;
-// const TIME_FOR_QUESTION = 30;
+const TIME_FOR_QUESTION = 30;
+const NUMBER_OF_GAME_LEVELS = 10;
 export const AnswerValue = {
   CORRECT: `correct`,
   WRONG: `wrong`,
@@ -15,24 +16,38 @@ const AnswerPoint = {
   [AnswerValue.SLOW]: 50
 };
 
-// export const getInitialState = () => Object.freeze({
-//   answers: [],
-//   lives: MAX_LIVES,
-//   question: 0,
-//   time: TIME_FOR_QUESTION
-// });
+export const getInitialState = () => Object.freeze({
+  answers: [],
+  lives: MAX_LIVES,
+  question: 0,
+  time: TIME_FOR_QUESTION
+});
 
+// Подсчет очков
 export const calculatePoints = (answers, lives) => {
   const wrongAnswers = answers.filter((elem) => elem === AnswerValue.WRONG).length;
-  const notAnsweredQuestions = ANSWER_NUMBER - answers.length;
+  const notAnsweredQuestions = NUMBER_OF_ANSWERS - answers.length;
   if (wrongAnswers + lives !== MAX_LIVES) {
     throw new Error(`the number of lives must match the number of errors`);
-  }
-  if (lives && notAnsweredQuestions) {
-    throw new Error(`game not ended`);
   }
   if (notAnsweredQuestions < 0) {
     throw new Error(`answers more than questions`);
   }
-  return answers.length === ANSWER_NUMBER ? answers.reduce((points, element) => points + AnswerPoint[element], lives * LEFT_LIVES_POINT) : -1;
+  if (lives && notAnsweredQuestions) {
+    throw new Error(`game not ended`);
+  }
+  return answers.length === NUMBER_OF_ANSWERS ? answers.reduce((points, element) => points + AnswerPoint[element], lives * LEFT_LIVES_POINT) : -1;
 };
+
+// Переключение уровней
+// export const changeLevel = (game, question) => {
+//   if (typeof game !== `object` || typeof level !== `number` || typeof game.level !== `number`) {
+//     throw new Error(`Parameters shouldn't be undefined or incorrect parameter type.`);
+//   }
+//   if (isNaN(question) || question < 0 || question > NUMBER_OF_GAME_LEVELS - 1) {
+//     throw new RangeError(`Level must be between 0...${NUMBER_OF_GAME_LEVELS - 1}.`);
+//   }
+//   return Object.assign({}, game, {
+//     question
+//   });
+// };
--- js/data/game.test.js
+++ js/data/game.test.js
@@ -4,7 +4,9 @@ import {
 } from 'chai';
 import {
   calculatePoints,
-  AnswerValue
+  AnswerValue,
+  changeLevel,
+  getInitialState
 } from './game.js';
 
 const testNineAnswers = [...new Array(6).fill(AnswerValue.CORRECT), ...new Array(3).fill(AnswerValue.WRONG)];
@@ -18,7 +20,7 @@ const noMistakesAnswers = [...testTenAnswers];
 const nineAnswersTwoMistakes = [...new Array(7).fill(AnswerValue.CORRECT), ...new Array(2).fill(AnswerValue.WRONG)];
 const answersMoreQuestions = new Array(11).fill(AnswerValue.CORRECT);
 
-describe(`Game`, () => {
+describe(`Calculate Points`, () => {
   describe(`should check if data correct`, () => {
     it(`the number of lives must match the number of errors`, () => {
       assert.throws(() => calculatePoints(threeMistakesAnswers, 1), /the number of lives must match the number of errors/);
@@ -47,5 +49,35 @@ describe(`Game`, () => {
     it(`if all answers are fast and all three lives are left should return 1650 points`, () => {
       assert.equal(calculatePoints(testMaxAnswers, 3), 1650);
     });
+  });
+});
+
+describe(`Change level`, () => {
+  it(`Parameters shouldn't be incorrect parameter type`, () => {
+    assert.throws(() => changeLevel({}, 1), /Parameters shouldn't be undefined or incorrect parameter type/);
+    assert.throws(() => changeLevel([], 1), /Parameters shouldn't be undefined or incorrect parameter type/);
+    assert.throws(() => changeLevel(0, 1), /Parameters shouldn't be undefined or incorrect parameter type/);
+    assert.throws(() => changeLevel(null, 1), /Parameters shouldn't be undefined or incorrect parameter type/);
+    assert.throws(() => changeLevel(NaN, 1), /Parameters shouldn't be undefined or incorrect parameter type/);
+    assert.throws(() => changeLevel(`string`, 1), /Parameters shouldn't be undefined or incorrect parameter type/);
+    assert.throws(() => changeLevel(undefined, 1), /Parameters shouldn't be undefined or incorrect parameter type/);
+    assert.throws(() => changeLevel(getInitialState(), {}), /Parameters shouldn't be undefined or incorrect parameter type/);
+    assert.throws(() => changeLevel(getInitialState(), []), /Parameters shouldn't be undefined or incorrect parameter type/);
+    assert.throws(() => changeLevel(getInitialState(), null), /Parameters shouldn't be undefined or incorrect parameter type/);
+    assert.throws(() => changeLevel(getInitialState(), `string`), /Parameters shouldn't be undefined or incorrect parameter type/);
+    assert.throws(() => changeLevel(getInitialState(), undefined), /Parameters shouldn't be undefined or incorrect parameter type/);
+  });
+  it(`should throw range error`, () => {
+    assert.throws(() => changeLevel(getInitialState(), -1), /Level must be between 0...9/);
+    assert.throws(() => changeLevel(getInitialState(), 10), /Level must be between 0...9/);
+    assert.throws(() => changeLevel(getInitialState(), NaN), /Level must be between 0...9/);
+  });
+  it(`should change level`, () => {
+    assert.equal(changeLevel(getInitialState(), 0).question, 0);
+    assert.equal(changeLevel(getInitialState(), 5).question, 5);
+    assert.equal(changeLevel(getInitialState(), 9).question, 9);
+  });
+  it(`check start value`, () => {
+    assert.equal(getInitialState().question, 3);
   });
 });
