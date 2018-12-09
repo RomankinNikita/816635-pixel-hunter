f44676354aa88b7af29ecc47a14d6506df1e8f7b
RomankinNikita <kryaxtun@gmail.com>
2018-12-10 02:15:14 +0400

    save11

--- js/game-screens/get-game-type.js
+++ js/game-screens/get-game-type.js
@@ -11,29 +11,22 @@ import modalConfirm from '../modal/modal-confirm/modal-confirm.js';
 
 const mainElement = document.querySelector(`#main`);
 
+const startTimer = (game) => {
+  game.onTick();
+  game.time -= 1;
+  if (game.time < 0) {
+    clearTimeout(game.timer);
+    const answer = AnswerValue.WRONG;
+    game.onAnswer(answer);
+  } else {
+    game.timer = setTimeout(startTimer, 1000);
+  }
+};
+
 const getGameType = (state, GameView) => {
   const gameType = new GameView(state);
 
-  gameType.setTimer = () => {
-    const startTimer = () => {
-      gameType.onTick();
-      gameType.time -= 1;
-      if (gameType.time < 0) {
-        clearTimeout(gameType.timer);
-        const answer = AnswerValue.WRONG;
-        gameType.onAnswer(answer);
-      } else {
-        gameType.timer = setTimeout(startTimer, 1000);
-      }
-    };
-    startTimer();
-  };
-
-  gameType.setTimer();
-
   gameType.onBackClick = () => {
-    console.log(1);
-
     mainElement.appendChild(modalConfirm().element);
   };
 
@@ -42,6 +35,8 @@ const getGameType = (state, GameView) => {
     const nextState = getNextState(gameType.state, answer);
     getNextScreen(nextState);
   };
+
+  startTimer(gameType);
 
   return gameType;
 };
