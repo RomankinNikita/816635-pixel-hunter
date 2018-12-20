a820a9c74d7fb33a64979ce64808ef05fe5adfb5
RomankinNikita <kryaxtun@gmail.com>
2018-12-20 20:34:49 +0400

    save1

--- js/data/settings.js
+++ js/data/settings.js
@@ -1,4 +1,5 @@
 const hash = window.location.hash.replace(`#`, ``);
 
-export const getDebugState = () => hash.toLowerCase() === `debug`;
 export const DEBUG_STYLE = `border: 3px solid red;`;
+
+export const getDebugState = () => hash.toLowerCase() === `debug`;
